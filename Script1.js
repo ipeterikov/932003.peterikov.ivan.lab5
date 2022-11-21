e24b3ceaae63b9f8028e9f487e89f627a1611645
ipeterikov <113367436+ipeterikov@users.noreply.github.com>
2022-11-21 17:02:00 +0700

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,25 @@
+function open1() {
+    document.getElementById('first').style.zIndex = "1";
+    document.getElementById('wc').style.zIndex = "1";
+}
+function open2() {
+    document.getElementById('second').style.zIndex = "1";
+    document.getElementById('wc').style.zIndex = "1";
+}
+function open3() {
+    document.getElementById('third').style.zIndex = "1";
+    document.getElementById('wc').style.zIndex = "1";
+}
+
+function close1() {
+    document.getElementById('first').style.zIndex = "-1";
+    document.getElementById('wc').style.zIndex = "-1";
+}
+function close2() {
+    document.getElementById('second').style.zIndex = "-1";
+    document.getElementById('wc').style.zIndex = "-1";
+}
+function close3() {
+    document.getElementById('third').style.zIndex = "-1";
+    document.getElementById('wc').style.zIndex = "-1";
+}
